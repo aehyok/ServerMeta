31a01c16c55f5edab4cd05038f5643a110fa369f
刘启明 <aehyok@vip.qq.com>
2017-08-10 14:34:03 +0800

    修改代码,使F5能够运行

--- SinoSZJS.CS.Server/Program.cs
+++ SinoSZJS.CS.Server/Program.cs
@@ -9,6 +9,7 @@ using SinoSZJS.WCF.Lib;
 using SinoSZJS.Base.SystemLog;
 using SinoSZJS.Base.Misc;
 using SinoSZJS.Base.UserLog;
+using SinoSZJS.DataAccess.Sql;
 
 namespace SinoSZJS.CS.Server
 {
@@ -21,8 +22,8 @@ namespace SinoSZJS.CS.Server
         {
             //	初始化服务器端配置        
             //建立系统日志和用户日志的写入器
-            SystemLogWriter.ICS_SystemLog = new OraSysLogWriter();
-            UserLogWriter.ICS_UserLog = new OraUserLogWriter();
+            //SystemLogWriter.ICS_SystemLog = new OraSysLogWriter();
+            //UserLogWriter.ICS_UserLog = new OraUserLogWriter();
 
             Dictionary<string, ServiceHost> hostLib = new Dictionary<string, ServiceHost>();
             hostLib.Add("AuthorizeService", new ServiceHost(typeof(AuthorizeService)));
@@ -34,15 +35,15 @@ namespace SinoSZJS.CS.Server
             if (ConfigFile.WindowsService)
             {
                 //启动服务
-                SystemLogWriter.WriteLog("开始启动服务！", EventLogEntryType.Information);
+                LogWriter.WriteSystemLog("开始启动服务！", "Info");
                 System.ServiceProcess.ServiceBase.Run(new SinoSZMainService(hostLib));
-                SystemLogWriter.WriteLog("服务结束！", EventLogEntryType.Information);
+                LogWriter.WriteSystemLog("服务结束！", "Info");
             }
             else
             {//启动程序
-                SystemLogWriter.WriteLog("启动程序完成！", EventLogEntryType.Information);
+                LogWriter.WriteSystemLog("启动程序完成！", "Info");
                 Application.Run(new frmMain("系统服务程序监控端", hostLib));
-                SystemLogWriter.WriteLog("程序结束！", EventLogEntryType.Information);
+                LogWriter.WriteSystemLog("程序结束！", "Info");
             }
 
         }
